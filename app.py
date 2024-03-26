69cfdee4dd904bdd1f96c29ba7f26b0d94a208b5
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-03-26 16:56:22 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,52 @@
 import streamlit as st
-from webcam import webcam_input
+from modules.cam import WebcamInput
+
+#
+if "exercise_type" not in st.session_state:
+    st.session_state.exercise_type = None
+if "pose" not in st.session_state:
+    st.session_state.pose = None
+if "dynamic" not in st.session_state:
+    st.session_state.dynamic = None
+if "show_keypoints" not in st.session_state:
+    st.session_state.show_keypoints = True
+
+
 
 def main():
-    
     # Sidebar
-    st.sidebar.image('src/logo.png', use_column_width=True)
-    st.sidebar.header('Ballettposen')
-    pose = st.sidebar.selectbox(
-    'Welche Position möchtest Du üben?',
-    ('1.Position', '1.Position', '3.Position', '4.Position', '5.Position'))
-    st.sidebar.info(f'Auswahl: {pose}')
-     
+    with st.sidebar:
+        st.image('src/logo.png', use_column_width=True)
+
+        # Auswahl der Übungsart
+        st.radio("Wähle den Übungstyp aus:",
+                 ("Statische Posen", "Ballett-Bewegungen"),
+                 key='exercise_type')
+
+        # statische Übungen
+        if st.session_state.exercise_type == "Statische Posen":
+            st.selectbox('Welche Position möchtest Du üben?',
+                         ('1. Position', '2. Position', '3. Position'),
+                         key='pose')
+            st.info(f'Auswahl: {st.session_state.pose}')
+        # Dynamische Übungen
+        else:
+            st.selectbox('Welche Bewegungen möchtest Du üben?',
+                         ('1. Bewegung', '2. Bewegung', '3. Bewegung'),
+                         key='dynamic')
+            st.info(f'Auswahl: {st.session_state.dynamic}')
+
+        # Toggle für Keypoints
+        st.toggle('Keypoints anzeigen', key='show_keypoints')
+
     # Hauptfenster
     st.title("🩰 Plié AI")
-    st.subheader("Pose Learning and Improvement Excercises with AI")
-    # Webcam
-    webcam_input()
+    st.subheader("Pose Learning and Improvement Exercises with AI")
 
+    # Webcam-Input
+    input_handler = WebcamInput()
+    input_handler.run()
 
 
 if __name__ == "__main__":
     main()
-
-    
-    
-
-
-
-
-
