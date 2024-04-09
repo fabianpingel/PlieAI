c351aa8ef97734922e525194debd8b06ccef0de4
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-04-09 12:38:55 +0200

    Update cam.py

--- modules/cam.py
+++ modules/cam.py
@@ -1,29 +1,18 @@
 # Imports
-# `av`-Moduls für die Verarbeitung von Audio- und Videodaten
-import av
-# `webrtc_streamer`-Funktion aus dem `streamlit_webrtc`-Modul für die Bereitstellung von WebRTC-Streams in Streamlit
-from streamlit_webrtc import webrtc_streamer
-# `streamlit`-Moduls, welches Funktionen und Widgets für die Erstellung der Streamlit-App bereitstellt
-import streamlit as st
-# Importieren der `PoseDetector`-Klasse aus dem `detector`-Modul für die Pose-Erkennung
-#from modules.detector import PoseDetector
-from modules.detector_holistic import PoseDetector_Holistic
-# Import der Funktion 'get_ice_servers' aus dem Modul 'utils.turn'
-from utils.turn import get_ice_servers
-# Importieren des `cv2`-Moduls für die Bildverarbeitung
-import cv2
-# Importieren der `PoseClassifier`-Klasse aus dem `classifier`-Modul für die Pose-Klassifizierung
-from modules.classifier import PoseClassifier
+import av                                       # für die Verarbeitung von Audio- und Videodaten
+import streamlit as st                          # für die Erstellung der Streamlit-App
+from streamlit_webrtc import webrtc_streamer    # für WebRTC-Streams in Streamlit
+from modules.detector import PoseDetector       # `PoseDetector`-Klasse für die Pose-Erkennung
+from utils.turn import get_ice_servers          # Funktion 'get_ice_servers' aus dem Modul 'utils.turn'
+import cv2                                      # OpenCV für die Bildverarbeitung
+from modules.classifier import PoseClassifier   # `PoseClassifier`-Klasse aus dem `classifier`-Modul für die Pose-Klassifizierung
 import logging
-
-##import threading
-
+import numpy as np
 
 
 st_webrtc_logger = logging.getLogger("streamlit_webrtc")
 st_webrtc_logger.setLevel(logging.WARNING)
 
-#lock = threading.Lock()
 
 class WebcamInput:
     """
@@ -34,9 +23,18 @@ class WebcamInput:
         """
         Initialisiert die WebcamInput-Klasse.
         """
-        self.pose_detector = PoseDetector_Holistic() # Initialisierung des PoseDetector-Objekts
-        #self.pose_detector = PoseDetector()  # Initialisierung des PoseDetector-Objekts
-        self.pose_classifier = PoseClassifier('Logistic Regression.pkl')
+        self.pose_detector = PoseDetector()  # Initialisierung des PoseDetector-Objekts
+        self.pose_classifier = PoseClassifier('k-Nearest Neighbors.pkl')
+        self.image_size = float(getattr(st.session_state, 'image_size', 100) / 100)
+        self.plot_3d_landmarks = getattr(st.session_state, 'plot_3d_landmarks', False)
+
+
+    def __del__(self):
+        # Freigeben der Ressourcen
+        self.pose_detector.close()
+        self.pose_classifier.close()
+
+
 
     def video_frame_callback(self, frame: av.VideoFrame) -> av.VideoFrame:
         """Callback-Funktion für jedes empfangene Video-Frame.
@@ -49,12 +47,19 @@ class WebcamInput:
         """
         # Konvertierung des Frames in ein Numpy-Array
         image = frame.to_ndarray(format="bgr24")
+
+        # Anpassen der Bildgröße auf
+        resized_image = cv2.resize(image, None, fx=self.image_size, fy=self.image_size)
+
         # Verarbeitung des Bildes durch den PoseDetector
-        processed_image, results = self.pose_detector.process_image(image)
+        processed_image, results = self.pose_detector.process_image(resized_image)
+
         # Verarbeitung des Bildes durch den PoseClassifier
-        processed_image = self.pose_classifier.process_image(processed_image, results)
+        if not self.plot_3d_landmarks:
+            processed_image = self.pose_classifier.process_image(processed_image, results)
+
         # Bild horizontal spiegeln für Selfie-Ansicht
-        #processed_image = cv2.flip(processed_image, 1)
+        # processed_image = cv2.flip(processed_image, 1)
 
         # Rückgabe des verarbeiteten Bildes als VideoFrame-Objekt
         return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
@@ -73,17 +78,5 @@ class WebcamInput:
         )
 
         if not webrtc_ctx.state.playing:
-            st.info("Beginne die Übung mit Klick auf 'Start'")
             st.warning("Warte auf Video-Stream...")  # Anzeige einer Warnung, wenn kein Video-Stream vorhanden ist
-
-        #st.slider("Dauer in Sekunden:", 0, 10, 0, 1)
-
-        #while webrtc_ctx.state.playing:
-        #    with lock:
-        #        print(train_dict['success'])
-        #        progress = train_dict['num_repeats']
-
-        #    if train_dict['success']:
-        #        st.success("Herzlichen Glückwunsch! Du hast die Pose erfolgreich gehalten!")
-
 
