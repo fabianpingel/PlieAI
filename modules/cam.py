023f730171c30ac751359514c2595a3687e8e77e
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-05-02 13:12:10 +0200

    Update cam.py

--- modules/cam.py
+++ modules/cam.py
@@ -45,23 +45,29 @@ class WebcamInput:
         Returns:
             av.VideoFrame: Das verarbeitete Video-Frame.
         """
-        # Konvertierung des Frames in ein Numpy-Array
-        image = frame.to_ndarray(format="bgr24")
-        #print(f'Input shape: {image.shape}')
-
-        # Anpassen der Bildgröße auf
-        resized_image = cv2.resize(image, None, fx=self.image_size, fy=self.image_size)
-        #print(f'Resized shape: {resized_image.shape}')
-
-        # Verarbeitung des Bildes durch den PoseDetector
-        processed_image, results = self.pose_detector.process_image(resized_image)
-
-        # Verarbeitung des Bildes durch den PoseClassifier
-        if not self.plot_3d_landmarks:
-            processed_image = self.pose_classifier.process_image(processed_image, results)
-
-        # Rückgabe des verarbeiteten Bildes als VideoFrame-Objekt
-        return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
+        try:
+            # Konvertierung des Frames in ein Numpy-Array
+            image = frame.to_ndarray(format="bgr24")
+            #print(f'Input shape: {image.shape}')
+    
+            # Anpassen der Bildgröße auf
+            resized_image = cv2.resize(image, None, fx=self.image_size, fy=self.image_size)
+            #print(f'Resized shape: {resized_image.shape}')
+    
+            # Verarbeitung des Bildes durch den PoseDetector
+            processed_image, results = self.pose_detector.process_image(resized_image)
+    
+            # Verarbeitung des Bildes durch den PoseClassifier
+            if not self.plot_3d_landmarks:
+                processed_image = self.pose_classifier.process_image(processed_image, results)
+    
+            # Rückgabe des verarbeiteten Bildes als VideoFrame-Objekt
+            return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
+            
+        except Exception as e:
+            self.logger.error(f" Fehler bei der Verarbeitung des Streams: {e}")
+            # Rückgabe des unbearbeiteten Frames im Fehlerfall
+            return frame 
 
 
     def run(self) -> None:
