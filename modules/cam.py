edd4f8aad66c81056377c25b2d2a72ea1443b735
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-05-06 14:33:01 +0200

    Add files via upload

--- modules/cam.py
+++ modules/cam.py
@@ -9,6 +9,11 @@ from modules.classifier import PoseClassifier   # `PoseClassifier`-Klasse aus de
 import logging
 import numpy as np
 
+st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+st_webrtc_logger.setLevel(logging.WARNING)
+
+aioice_logger = logging.getLogger("aioice")
+aioice_logger.setLevel(logging.WARNING)
 
 class WebcamInput:
     """
@@ -19,6 +24,8 @@ class WebcamInput:
         """
         Initialisiert die WebcamInput-Klasse.
         """
+
+
         self.pose_detector = PoseDetector()  # Initialisierung des PoseDetector-Objekts
         self.pose_classifier = PoseClassifier('Logistic Regression.pkl')
         self.image_size = float(getattr(st.session_state, 'image_size', 100) / 100)
@@ -49,25 +56,26 @@ class WebcamInput:
             # Konvertierung des Frames in ein Numpy-Array
             image = frame.to_ndarray(format="bgr24")
             #print(f'Input shape: {image.shape}')
-    
+
             # Anpassen der Bildgröße auf
             resized_image = cv2.resize(image, None, fx=self.image_size, fy=self.image_size)
             #print(f'Resized shape: {resized_image.shape}')
-    
+
             # Verarbeitung des Bildes durch den PoseDetector
             processed_image, results = self.pose_detector.process_image(resized_image)
-    
+
             # Verarbeitung des Bildes durch den PoseClassifier
             if not self.plot_3d_landmarks:
                 processed_image = self.pose_classifier.process_image(processed_image, results)
-    
+
             # Rückgabe des verarbeiteten Bildes als VideoFrame-Objekt
             return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
-            
+
         except Exception as e:
             self.logger.error(f" Fehler bei der Verarbeitung des Streams: {e}")
             # Rückgabe des unbearbeiteten Frames im Fehlerfall
-            return frame 
+            return frame
+
 
 
     def run(self) -> None:
@@ -82,7 +90,7 @@ class WebcamInput:
             media_stream_constraints={"video": {
                                             "width": {"exact": 640},
                                             "height": {"exact": 480},
-                                            "frameRate": {"ideal": 30}},
+                                            "frameRate": {"ideal": 20}},
                                       "audio": False},
             async_processing=True,
         )
