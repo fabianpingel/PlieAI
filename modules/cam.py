796c5a0f0e6f5c6de6bf81aaf7bb36012e34a74f
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-05-08 12:57:04 +0200

    Update cam.py

--- modules/cam.py
+++ modules/cam.py
@@ -82,13 +82,14 @@ class WebcamInput:
         webrtc_ctx = webrtc_streamer(
             key="pose_detection",
             mode=WebRtcMode.SENDRECV,
-            rtc_configuration={"iceServers": get_ice_servers()},
+            rtc_configuration={"iceServers": get_ice_servers(),
+                               "iceTransportPolicy": "relay",},
             video_frame_callback=self.video_frame_callback,
             media_stream_constraints={"video": {
                                             "width": {"exact": 640},
                                             "height": {"exact": 480},
                                             "frameRate": {"ideal": 20}},
-                                      "audio": False},
+                                      "audio": False},           
             async_processing=True,
         )
 
