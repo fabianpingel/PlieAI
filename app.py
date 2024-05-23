b5d0136be9a819df7eea1771984c9bb0354c65a7
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-05-23 19:37:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,14 +49,15 @@ def main():
             #st.toggle("Face Landmarks", value=True, key='face_landmarks')
             #st.toggle("Hand Landmarks", value=True, key='hand_landmarks')
             #st.toggle("Pose Landmarks", value=True, key='pose_landmarks')
-            st.toggle("Selfie-Ansicht", value=False, key='selfie')
-            st.toggle("Hintergrund entfernen", value=False, key='segmentation')
-            st.toggle("Pose Abweichungen", value=True, key='deviation')
-            st.select_slider('Größe in %', options=list(range(100, 201, 50)), value=150, key='image_size')
+            st.toggle("Selfie-Ansicht", value=True, key='selfie', help='Hier kann die Kamera in den Selfie-Modus umgestellt werden.')
+            st.toggle("Hintergrund entfernen", value=False, key='segmentation', help='Hier kann der Hintergrund entfernbt werden.')
+            st.toggle("Pose Abweichungen", value=True, key='deviation', help='Hier kann die Anzeige der Poseabweichungen ein-/ausgestellt werden.')
+            st.select_slider('Größe in %', options=list(range(100, 201, 50)), value=150, key='image_size', help='Hier kann die Schriftgröße geändert werden.')
+            st.color_picker('Textfarbe wählen', value='#FFFFFF', key='text_color', help='Hier kann die Farbe der Texte eingestellt werden.')
 
         # Copyright und Version
         copyright = "© 2024 Fabian Pingel"
-        version = "Beta-Version 1.4"
+        version = "Beta-Version 1.5"
 
         # Sidebar
         st.sidebar.markdown(f"{copyright}\n{version}")
@@ -64,11 +65,21 @@ def main():
     # Hauptfenster
     st.title("🩰 Plié AI")
     st.subheader("Pose Learning and Improvement Exercises with AI")
-    st.markdown("🥳 16.05.2024 - Prototyp bereit zum Testen... 🎉")
+    st.markdown("🎞️ 23.05.2024 - Möglichkeit der Videoanalyse hinzugefügt...")
+
+    # Auswahl der Eingabe (Kamera oder Video)
+    st.radio("Medienquelle auswählen:",
+             ["Stream :clapper:", "Video :video_camera:"],
+             key='source',
+             help="Hier kann die Medienquelle LiveStream oder Video ausgewählt werden",
+             horizontal=True)
 
     # Webcam-Input
     input_handler = WebcamInput()
-    input_handler.run()
+    if st.session_state.source == 'Stream :clapper:':
+        input_handler.run()
+    else:
+        input_handler.run_video()
 
 
 if __name__ == "__main__":
